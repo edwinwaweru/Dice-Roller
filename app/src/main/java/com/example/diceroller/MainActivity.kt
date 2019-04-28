d14838306c25a0b2ff5333d48499a3d21550f879
Your Name <mihangokennedy@gmail.com>
2019-04-28 03:27:03 +0300

    Added dice images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +15,12 @@ class MainActivity : AppCompatActivity() {
         roll_button.text = getString(R.string.let_roll)
 
         roll_button.setOnClickListener {
-            Toast.makeText(
-                this,
-                "Hello Rolling",
-                Toast.LENGTH_LONG
-        ).show() }
+            Toast.makeText(this, "Hello Rolling", Toast.LENGTH_LONG).show()
+            rollDice()}
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        result_text.text = "$randomInt"
     }
 }
